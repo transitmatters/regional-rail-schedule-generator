0fb2495cb36f6fad1f52be7b9ddea2cd2fe8f0b6
vickymmcd <vickymmcd@gmail.com>
2025-07-22 10:24:48 -0400

    adding bus routes and regenerate

--- network/main.py
+++ network/main.py
@@ -9,20 +9,9 @@ LARGE_RECURSION_LIMIT = 10000
 
 
 def get_gtfs_network():
-    if os.path.exists(PATH_TO_PICKLED_NETWORK):
-        with open(PATH_TO_PICKLED_NETWORK, "rb") as file:
-            try:
-                return pickle.load(file)
-            except Exception:
-                print("Error loading pickled network.")
     print("Creating network from scratch...")
     network = build_network_from_gtfs()
-    with open(PATH_TO_PICKLED_NETWORK, "wb") as file:
-        old_limit = sys.getrecursionlimit()
-        sys.setrecursionlimit(LARGE_RECURSION_LIMIT)
-        pickle.dump(network, file)
-        sys.setrecursionlimit(old_limit)
-        return network
+    return network
 
 
 if __name__ == "__main__":
